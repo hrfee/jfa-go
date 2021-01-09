d9e1e2f58b0a6bc742d7033c3869f043a691174f
Harvey Tindall <hrfee@protonmail.ch>
2021-01-09 20:38:13 +0000

    compiles, basic issues fixed
    Server type is found under the Jellyfin settings tab, where you can
    change it to emby. Currently:
    
    * logs in
    * creates users
    * parses accounts

--- mediabrowser/jfapi.go
+++ mediabrowser/jfapi.go
@@ -1,181 +1,13 @@
 package mediabrowser
 
 import (
-	"bytes"
-	"compress/gzip"
 	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
-	"strings"
 	"time"
-
-	"github.com/hrfee/jfa-go/common"
 )
 
-// NewJellyfin returns a new Jellyfin object.
-func NewJellyfin(server, client, version, device, deviceID string, timeoutHandler common.TimeoutHandler, cacheTimeout int) (*MediaBrowserStruct, error) {
-	jf := &MediaBrowserStruct{}
-	jf.Server = server
-	jf.client = client
-	jf.version = version
-	jf.device = device
-	jf.deviceID = deviceID
-	jf.useragent = fmt.Sprintf("%s/%s", client, version)
-	jf.timeoutHandler = timeoutHandler
-	jf.auth = fmt.Sprintf("MediaBrowser Client=\"%s\", Device=\"%s\", DeviceId=\"%s\", Version=\"%s\"", client, device, deviceID, version)
-	jf.header = map[string]string{
-		"Accept":               "application/json",
-		"Content-type":         "application/json; charset=UTF-8",
-		"X-Application":        jf.useragent,
-		"Accept-Charset":       "UTF-8,*",
-		"Accept-Encoding":      "gzip",
-		"User-Agent":           jf.useragent,
-		"X-Emby-Authorization": jf.auth,
-	}
-	jf.httpClient = &http.Client{
-		Timeout: 10 * time.Second,
-	}
-	infoURL := fmt.Sprintf("%s/System/Info/Public", server)
-	req, _ := http.NewRequest("GET", infoURL, nil)
-	resp, err := jf.httpClient.Do(req)
-	defer jf.timeoutHandler()
-	if err == nil {
-		data, _ := ioutil.ReadAll(resp.Body)
-		json.Unmarshal(data, &jf.ServerInfo)
-	}
-	jf.cacheLength = cacheTimeout
-	jf.CacheExpiry = time.Now()
-	return jf, nil
-}
-
-// Authenticate attempts to authenticate using a username & password
-func (jf *MediaBrowserStruct) Authenticate(username, password string) (map[string]interface{}, int, error) {
-	jf.Username = username
-	jf.password = password
-	jf.loginParams = map[string]string{
-		"Username": username,
-		"Pw":       password,
-		"Password": password,
-	}
-	buffer := &bytes.Buffer{}
-	encoder := json.NewEncoder(buffer)
-	encoder.SetEscapeHTML(false)
-	err := encoder.Encode(jf.loginParams)
-	if err != nil {
-		return nil, 0, err
-	}
-	// loginParams, _ := json.Marshal(jf.loginParams)
-	url := fmt.Sprintf("%s/Users/authenticatebyname", jf.Server)
-	req, err := http.NewRequest("POST", url, buffer)
-	defer jf.timeoutHandler()
-	if err != nil {
-		return nil, 0, err
-	}
-	for name, value := range jf.header {
-		req.Header.Add(name, value)
-	}
-	resp, err := jf.httpClient.Do(req)
-	if err != nil || resp.StatusCode != 200 {
-		return nil, resp.StatusCode, err
-	}
-	defer resp.Body.Close()
-	var data io.Reader
-	switch resp.Header.Get("Content-Encoding") {
-	case "gzip":
-		data, _ = gzip.NewReader(resp.Body)
-	default:
-		data = resp.Body
-	}
-	var respData map[string]interface{}
-	json.NewDecoder(data).Decode(&respData)
-	jf.AccessToken = respData["AccessToken"].(string)
-	user := respData["User"].(map[string]interface{})
-	jf.userID = respData["User"].(map[string]interface{})["Id"].(string)
-	jf.auth = fmt.Sprintf("MediaBrowser Client=\"%s\", Device=\"%s\", DeviceId=\"%s\", Version=\"%s\", Token=\"%s\"", jf.client, jf.device, jf.deviceID, jf.version, jf.AccessToken)
-	jf.header["X-Emby-Authorization"] = jf.auth
-	jf.Authenticated = true
-	return user, resp.StatusCode, nil
-}
-
-func (jf *MediaBrowserStruct) get(url string, params map[string]string) (string, int, error) {
-	var req *http.Request
-	if params != nil {
-		jsonParams, _ := json.Marshal(params)
-		req, _ = http.NewRequest("GET", url, bytes.NewBuffer(jsonParams))
-	} else {
-		req, _ = http.NewRequest("GET", url, nil)
-	}
-	for name, value := range jf.header {
-		req.Header.Add(name, value)
-	}
-	resp, err := jf.httpClient.Do(req)
-	defer jf.timeoutHandler()
-	if err != nil || resp.StatusCode != 200 {
-		if resp.StatusCode == 401 && jf.Authenticated {
-			jf.Authenticated = false
-			_, _, authErr := jf.Authenticate(jf.Username, jf.password)
-			if authErr == nil {
-				v1, v2, v3 := jf.get(url, params)
-				return v1, v2, v3
-			}
-		}
-		return "", resp.StatusCode, err
-	}
-	defer resp.Body.Close()
-	var data io.Reader
-	encoding := resp.Header.Get("Content-Encoding")
-	switch encoding {
-	case "gzip":
-		data, _ = gzip.NewReader(resp.Body)
-	default:
-		data = resp.Body
-	}
-	buf := new(strings.Builder)
-	io.Copy(buf, data)
-	//var respData map[string]interface{}
-	//json.NewDecoder(data).Decode(&respData)
-	return buf.String(), resp.StatusCode, nil
-}
-
-func (jf *MediaBrowserStruct) post(url string, data map[string]interface{}, response bool) (string, int, error) {
-	params, _ := json.Marshal(data)
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(params))
-	for name, value := range jf.header {
-		req.Header.Add(name, value)
-	}
-	resp, err := jf.httpClient.Do(req)
-	defer jf.timeoutHandler()
-	if err != nil || resp.StatusCode != 200 {
-		if resp.StatusCode == 401 && jf.Authenticated {
-			jf.Authenticated = false
-			_, _, authErr := jf.Authenticate(jf.Username, jf.password)
-			if authErr == nil {
-				v1, v2, v3 := jf.post(url, data, response)
-				return v1, v2, v3
-			}
-		}
-		return "", resp.StatusCode, err
-	}
-	if response {
-		defer resp.Body.Close()
-		var outData io.Reader
-		switch resp.Header.Get("Content-Encoding") {
-		case "gzip":
-			outData, _ = gzip.NewReader(resp.Body)
-		default:
-			outData = resp.Body
-		}
-		buf := new(strings.Builder)
-		io.Copy(buf, outData)
-		return buf.String(), resp.StatusCode, nil
-	}
-	return "", resp.StatusCode, nil
-}
-
-// DeleteUser deletes the user corresponding to the provided ID.
-func (jf *MediaBrowserStruct) DeleteUser(userID string) (int, error) {
+func jfDeleteUser(jf *MediaBrowser, userID string) (int, error) {
 	url := fmt.Sprintf("%s/Users/%s", jf.Server, userID)
 	req, _ := http.NewRequest("DELETE", url, nil)
 	for name, value := range jf.header {
@@ -186,8 +18,7 @@ func (jf *MediaBrowserStruct) DeleteUser(userID string) (int, error) {
 	return resp.StatusCode, err
 }
 
-// GetUsers returns all (visible) users on the Jellyfin instance.
-func (jf *MediaBrowserStruct) GetUsers(public bool) ([]map[string]interface{}, int, error) {
+func jfGetUsers(jf *MediaBrowser, public bool) ([]map[string]interface{}, int, error) {
 	var result []map[string]interface{}
 	var data string
 	var status int
@@ -216,8 +47,7 @@ func (jf *MediaBrowserStruct) GetUsers(public bool) ([]map[string]interface{}, i
 	return jf.userCache, 200, nil
 }
 
-// UserByName returns the user corresponding to the provided username.
-func (jf *MediaBrowserStruct) UserByName(username string, public bool) (map[string]interface{}, int, error) {
+func jfUserByName(jf *MediaBrowser, username string, public bool) (map[string]interface{}, int, error) {
 	var match map[string]interface{}
 	find := func() (map[string]interface{}, int, error) {
 		users, status, err := jf.GetUsers(public)
@@ -239,8 +69,7 @@ func (jf *MediaBrowserStruct) UserByName(username string, public bool) (map[stri
 	return match, status, err
 }
 
-// UserByID returns the user corresponding to the provided ID.
-func (jf *MediaBrowserStruct) UserByID(userID string, public bool) (map[string]interface{}, int, error) {
+func jfUserByID(jf *MediaBrowser, userID string, public bool) (map[string]interface{}, int, error) {
 	if jf.CacheExpiry.After(time.Now()) {
 		for _, user := range jf.userCache {
 			if user["Id"].(string) == userID {
@@ -273,8 +102,7 @@ func (jf *MediaBrowserStruct) UserByID(userID string, public bool) (map[string]i
 	return result, status, nil
 }
 
-// NewUser creates a new user with the provided username and password.
-func (jf *MediaBrowserStruct) NewUser(username, password string) (map[string]interface{}, int, error) {
+func jfNewUser(jf *MediaBrowser, username, password string) (map[string]interface{}, int, error) {
 	url := fmt.Sprintf("%s/Users/New", jf.Server)
 	stringData := map[string]string{
 		"Name":     username,
@@ -293,8 +121,7 @@ func (jf *MediaBrowserStruct) NewUser(username, password string) (map[string]int
 	return recv, status, nil
 }
 
-// SetPolicy sets the access policy for the user corresponding to the provided ID.
-func (jf *MediaBrowserStruct) SetPolicy(userID string, policy map[string]interface{}) (int, error) {
+func jfSetPolicy(jf *MediaBrowser, userID string, policy map[string]interface{}) (int, error) {
 	url := fmt.Sprintf("%s/Users/%s/Policy", jf.Server, userID)
 	_, status, err := jf.post(url, policy, false)
 	if err != nil || status != 200 {
@@ -303,15 +130,13 @@ func (jf *MediaBrowserStruct) SetPolicy(userID string, policy map[string]interfa
 	return status, nil
 }
 
-// SetConfiguration sets the configuration (part of homescreen layout) for the user corresponding to the provided ID.
-func (jf *MediaBrowserStruct) SetConfiguration(userID string, configuration map[string]interface{}) (int, error) {
+func jfSetConfiguration(jf *MediaBrowser, userID string, configuration map[string]interface{}) (int, error) {
 	url := fmt.Sprintf("%s/Users/%s/Configuration", jf.Server, userID)
 	_, status, err := jf.post(url, configuration, false)
 	return status, err
 }
 
-// GetDisplayPreferences gets the displayPreferences (part of homescreen layout) for the user corresponding to the provided ID.
-func (jf *MediaBrowserStruct) GetDisplayPreferences(userID string) (map[string]interface{}, int, error) {
+func jfGetDisplayPreferences(jf *MediaBrowser, userID string) (map[string]interface{}, int, error) {
 	url := fmt.Sprintf("%s/DisplayPreferences/usersettings?userId=%s&client=emby", jf.Server, userID)
 	data, status, err := jf.get(url, nil)
 	if err != nil || !(status == 204 || status == 200) {
@@ -325,8 +150,7 @@ func (jf *MediaBrowserStruct) GetDisplayPreferences(userID string) (map[string]i
 	return displayprefs, status, nil
 }
 
-// SetDisplayPreferences sets the displayPreferences (part of homescreen layout) for the user corresponding to the provided ID.
-func (jf *MediaBrowserStruct) SetDisplayPreferences(userID string, displayprefs map[string]interface{}) (int, error) {
+func jfSetDisplayPreferences(jf *MediaBrowser, userID string, displayprefs map[string]interface{}) (int, error) {
 	url := fmt.Sprintf("%s/DisplayPreferences/usersettings?userId=%s&client=emby", jf.Server, userID)
 	_, status, err := jf.post(url, displayprefs, false)
 	if err != nil || !(status == 204 || status == 200) {
--- setup.go
+++ setup.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hrfee/jfa-go/common"
-	"github.com/hrfee/jfa-go/jfapi"
+	"github.com/hrfee/jfa-go/mediabrowser"
 )
 
 type testReq struct {
@@ -15,7 +15,7 @@ type testReq struct {
 func (app *appContext) TestJF(gc *gin.Context) {
 	var req testReq
 	gc.BindJSON(&req)
-	tempjf, _ := jfapi.NewJellyfin(req.Host, "jfa-go-setup", app.version, "auth", "auth", common.NewTimeoutHandler("authJF", req.Host, true), 30)
+	tempjf, _ := mediabrowser.NewServer(mediabrowser.JellyfinServer, req.Host, "jfa-go-setup", app.version, "auth", "auth", common.NewTimeoutHandler("authJF", req.Host, true), 30)
 	_, status, err := tempjf.Authenticate(req.Username, req.Password)
 	if !(status == 200 || status == 204) || err != nil {
 		app.info.Printf("Auth failed with code %d (%s)", status, err)
