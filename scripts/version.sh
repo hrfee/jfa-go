f72960635dc3948b2303032e2fb28b323f4ecec0
Harvey Tindall <hrfee@protonmail.ch>
2023-06-26 23:48:16 +0100

    build: include debug symbols & sourcemaps in unstable builds
    should help with debugging.

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,6 +2,18 @@
 # sets version environment variable for goreleaser to use
 # scripts/version.sh goreleaser ...
 
+if [[ -z "${JFA_GO_SNAPSHOT}" ]]; then
+    export JFA_GO_SOURCEMAP=""
+    export JFA_GO_COPYTS="echo skipping sourcemaps"
+    export JFA_GO_STRIP=""
+    export JFA_GO_MINIFY="--minify"
+else
+    echo "SNAPSHOT"
+    export JFA_GO_SOURCEMAP="--sourcemap"
+    export JFA_GO_COPYTS="cp -r tempts data/web/js/ts"
+    export JFA_GO_STRIP="-s -w"
+    export JFA_GO_MINIFY=""
+fi
 
 JFA_GO_VERSION=$(git describe --exact-match HEAD 2> /dev/null || echo 'vgit')
 JFA_GO_CSS_VERSION="v3" JFA_GO_NFPM_EPOCH=$(git rev-list --all --count) JFA_GO_BUILD_TIME=$(date +%s) JFA_GO_BUILT_BY=${JFA_GO_BUILT_BY:-"???"} JFA_GO_VERSION="$(echo $JFA_GO_VERSION | sed 's/v//g')" $@
