448955c915272f1297eadfee713497599e09d3e9
Harvey Tindall <hrfee@protonmail.ch>
2024-08-04 15:40:50 +0100

    discord: ensure discord housekeeping enable for previous feature
    was only enabled when "require unique" was set, now also is if the new
    "disable_enable_role" is set.

--- housekeeping-d.go
+++ housekeeping-d.go
@@ -131,7 +131,7 @@ func newHousekeepingDaemon(interval time.Duration, app *appContext) *GenericDaem
 	d.Name("Housekeeping daemon")
 
 	clearEmail := app.config.Section("email").Key("require_unique").MustBool(false)
-	clearDiscord := app.config.Section("discord").Key("require_unique").MustBool(false)
+	clearDiscord := app.config.Section("discord").Key("require_unique").MustBool(false) || app.config.Section("discord").Key("disable_enable_role").MustBool(false)
 	clearTelegram := app.config.Section("telegram").Key("require_unique").MustBool(false)
 	clearMatrix := app.config.Section("matrix").Key("require_unique").MustBool(false)
 	clearPWR := app.config.Section("captcha").Key("enabled").MustBool(false) && !app.config.Section("captcha").Key("recaptcha").MustBool(false)
